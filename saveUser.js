1099e20a144b0241516b31868094b1e69d916606
Abhijeet <abhijeetdixit27@gmail.com>
2016-10-20 22:44:51 +0530

    Changing saveUser to a standalone service and adding a catch all route to return code 404

--- saveUser.js
+++ saveUser.js
@@ -1,4 +1,4 @@
-var firebase = require('firebase');
+/*var firebase = require('firebase');
 
 //Initializing
 var config = {
@@ -8,3 +8,24 @@ var config = {
 	storageBucket: "<BUCKET>.appspot.com",
 };
 firebase.initializeApp(config);
+*/
+
+var http = require('http'),
+	express = require('express'),
+	path = require('path');
+
+var app = express();
+app.set('port', process.env.PORT || 8081);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+	res.send('<html><body><h1>Hello World</h1></body></html>');
+});
+
+app.use(function (req, res) {
+	res.send('404. Not Found');
+});
+
+http.createServer(app).listen(app.get('port'), function () {
+	console.log('Server listening to port ' + app.get('port'));
+});
